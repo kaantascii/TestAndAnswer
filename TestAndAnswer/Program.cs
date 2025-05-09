aa872ea88808f4291375e1de7b84cbaf6e9109f6
Kaan Taşcı <k_mert@windowslive.com>
2025-05-09 18:34:15 +0300

    Add project files.

---
+++ TestAndAnswer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class soruCevap
+{
+    static void Main(string[] args)
+    {   // İlk Soru
+        Console.WriteLine("Soru: Hangi hayvan uçar?");
+        Console.WriteLine("A) Kedi");
+        Console.WriteLine("B) Kartal");
+       
+        string cevap = Console.ReadLine();
+        if (cevap.ToUpper() == "B")
+        {
+            Console.WriteLine("Doğru cevap!");
+        }
+        else
+        {
+            Console.WriteLine("Yanlış cevap. Doğru cevap B) Kartal.");
+        }
+        //İkinci Soru
+        Console.WriteLine("Soru: Hangi hayvan yüzebilir?");
+        Console.WriteLine("A) Balık");
+        Console.WriteLine("B) Aslan");
+
+        cevap = Console.ReadLine();
+        if (cevap.ToUpper() == "A")
+        {
+            Console.WriteLine("Doğru cevap!");
+        }
+        else
+        {
+            Console.WriteLine("Yanlış cevap. Doğru cevap A) Balık.");
+        }
+        // Üçüncü Soru
+        Console.WriteLine("Soru: Hangi hayvan yürüyebilir?");
+        Console.WriteLine("A) Yılan");
+        Console.WriteLine("B) Fil");
+        cevap = Console.ReadLine();
+        if (cevap.ToUpper() == "B")
+        {
+            Console.WriteLine("Doğru cevap!");
+        }
+        else
+        {
+            Console.WriteLine("Yanlış cevap. Doğru cevap B) Fil.");
+        }
+
+
+    }
+}
